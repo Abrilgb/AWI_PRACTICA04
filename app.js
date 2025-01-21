911b1994d7ea19e013a390039c3f814679fc6daa
Abril Guzman Barrera <230350@utxicotepec.edu.mx>
2025-01-20 21:41:14 -0600

    Practica 04

--- app.js
+++ app.js
@@ -1,103 +1,104 @@
-//exportacion de librerias 
-import express from 'express';
-import session from 'express-session';
-import bodyPatser from 'body-parse';
-import {v4 as uuidv4} from 'uuid';
-import os from 'os';
+// Exportación de librerías   
+import express from 'express';  
+import session from 'express-session';  
+import bodyParser from 'body-parser';  
+import { v4 as uuidv4 } from 'uuid';  
+import os from 'os';  
 
-const app=express();
-const PORT=3000;
+const app = express();  
+const PORT = 3000;  
 
-app.listen(PORT,()=>{
-    console.log(`Server iniciado en http://localhost:${PORT}`);
-});
+app.listen(PORT, () => {  
+    console.log(`Server iniciado en http://localhost:${PORT}`);  
+});  
 
-app.use(express.json());
-app.use(express.urlenconded({extended:true}));
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: true }));  
 
-//Sesiones almacenadas de Memoria RAM 
+// Sesiones almacenadas en memoria RAM   
+const sessions = {};  
+app.use(session({  
+    secret: "P4-AGB#yinyerina-sesionesHTTP-VariablesDeSesion",  
+    resave: false,  
+    saveUninitialized: true,  
+    cookie: { maxAge: 5 * 60 * 1000 }  
+}));  
 
-//const sessions ={}
-app.use({
-    secret: "P4-AGB#yinyerina-sesionesHTTP-VariablesDeSesion",
-    resave:false,
-    saveUninitialized:true,
-    cookie:{maxAge:5*60*1000}
-});
+app.get('/', (req, res) => {  
+    return res.status(200).json({  
+        message: 'Bienvenido al API de control de Sesiones',  
+        author: 'Abril Guzmán Barrera'  
+    });  
+});  
 
-app.get('/',(req,res)=>{
-    return res.status(200).json({
-        message: 'Bienvenido al API de control de Sesiones',
-        author: 'Abril Guzmán Barrera'
-    });
-});
+// Función de utilidad que nos permite acceder a la información de la interfaz de red   
+const getLocalIp = () => {  
+    const networkInterfaces = os.networkInterfaces();  
+    for (const interfaceName in networkInterfaces) {  
+        const interfaces = networkInterfaces[interfaceName];  
+        for (const iface of interfaces) {  
+            // Verificamos si la interfaz es interna y si es IPv4  
+            if (iface.family === "IPv4" && !iface.internal) {  
+                return iface.address;  
+            }  
+        }  
+    }  
+    return null; // Retornamos null si no se encuentra una interfaz de red válida   
+};  
 
-//funcion de utilida que nos permite acceder a la informacion de la interfaz de red 
-const getLocalIp=()=>{
-    const networkInterfaces=os.networkInterfaces();
-    for (const interfaceName in networkInterfaces){
-        const interfaces = networkInterfaces[interfaceName];
-        for (const iface of interfaces ){
-            //verificamos si la interfaz es internba y si es iPv4
-            if (iface.family === "IPv4" && !iface.internal){
-                return iface.address;
-            } 
-        }
-    }
-    return null ; // returnamos null si no se encuentra una interfaz de red valida 
-};
+app.post('/login', (req, res) => {  
+    const { email, nickname, macAddress } = req.body;  
+    if (!email || !nickname || !macAddress) {  
+        return res.status(400).json({  
+            message: "Se esperan campos requeridos"  
+        });  
+    }  
+    const sessionId = uuidv4();  
+    const now = new Date();  
 
-app.post('/login',(req,res)=>{
-    const { email, nickname, macAddress } = req.body;
-    if (!email || !nickname || !macAddress) {
-        return res.status(400).json({
-            message: "Se esperan campos requeridos" });
-      }
-      const sessionId = uuidv4();
-      const now = new Date();
-    
-      sessions[sessionId] = {
-        sessionId,
-        email,
-        nickname,
-        macAddress,
-        ip: getLocalIp(req),
-        createdAt: now,
-        lastAccessedAt: now
-    };
+    sessions[sessionId] = {  
+        sessionId,  
+        email,  
+        nickname,  
+        macAddress,  
+        ip: getLocalIp(),  
+        createdAt: now,  
+        lastAccessedAt: now  
+    };  
 
-    res.status(200).json({
-        message: "Inicio de sesión exitoso",
-        sessionId
-      });
-});
+    res.status(200).json({  
+        message: "Inicio de sesión exitoso",  
+        sessionId  
+    });  
+});  
 
+app.post("/logout", (req, res) => {  
+    const { sessionId } = req.body;  
+    if (!sessionId || !sessions[sessionId]) {  
+        return res.status(404).json({  
+            message: "No se ha encontrado una sesión activa."  
+        });  
+    }  
+    delete sessions[sessionId];  
+    res.status(200).json({  
+        message: "Logout echo"  
+    });  
+});  
 
-app.post("/logout", (req, res) => {
-    const { sessionId } = req.body;
-    if (!sessionId || !sessions[sessionId]) {
-      return res.status(404).json({ 
-        message: "No se ha encontrado una sesión activa." });
-    }
-    delete sessions[sessionId];
-    res.status(200).json({ 
-        message: "Logout echo" });
-  });
- 
+app.post("/update", (req, res) => {  
+    // Implementar lógica de actualización aquí  
+});  
 
+app.get("/status", (req, res) => {  
+    const sessionId = req.query.sessionId;  
+    if (!sessionId || !sessions[sessionId]) {  
+        return res.status(404).json({  
+            message: "No hay una sesión activa"  
+        });  
+    }  
 
-app.post("/update", (req, res) => {
-});
-
-app.get("status",(req,res)=>{
-    const sessionId= req.query.sessionId
-    if (!sessionId || !sessions[sessionId]) {
-      return res.status(404).json({ 
-        message: "No hay una sesion activa" });
-    }
-  
-    res.status(200).json({
-      message: "Sesión actualizada correctamente",
-      session: sessions[sessionId]
-    });
+    res.status(200).json({  
+        message: "Sesión actualizada correctamente",  
+        session: sessions[sessionId]  
+    });  
 });
